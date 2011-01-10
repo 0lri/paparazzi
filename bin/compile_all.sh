ce8ea871b9b20520f971d8a1477e5eb452bb00cd
0lri <Oliver.Riesener@hs-bremen.de>
2011-01-10 18:15:58 +0100

    airframes HB_TINY_RAZOR and HB_MINI_FIXEDWING ap.compile, sim.compile works again, sim is still infradred_adc_

--- bin/compile_all.sh
+++ bin/compile_all.sh
@@ -2,28 +2,23 @@
 #
 # $Id$
 #
-# SAS Busca_Razor_40cm Busca_Razor_80cm TWOG_CHNI Lerche MJ5 Busca2
-for i in SAS Busca_Razor_40cm Busca_Razor_80cm TWOG_CHNI Lerche MJ5 Busca2 HB_MINI
+# HB_SAS Busca_Razor_40cm Busca_Razor_80cm TWOG_CHNI Lerche MJ5 Busca2
+for i in HB_TINY_RAZOR HB_MINI_FIXEDWING MJ5
 do
 	echo "###################### $i #######################"
 	(
-		make AIRCRAFT=$i clean_ac ap.compile
+		echo make -f Makefile.ac AIRCRAFT=$i clean_ac ap.compile > /dev/tty
+		make -f Makefile.ac AIRCRAFT=$i clean_ac ap.compile
+		echo make -f Makefile.ac AIRCRAFT=$i clean_ac sim.compile > /dev/tty
+		make -f Makefile.ac AIRCRAFT=$i clean_ac sim.compile
 	) 2>&1 | egrep Total\|Error\|Fehler
 done
 
-for i in 1 2 3 4 # defect 5
-do
-	echo "###################### demo$i #######################"
-	(
-		make AIRCRAFT=DEMO clean_ac demo$i.compile
-	) 2>&1 | egrep Total\|Error\|Fehler
-done
-
-# Busca1
-# Busca1Razor
-# Busca2G
-# Busca2GL
-# Busca2L
-# Busca3
-# DEMO
-# MJ5
+#for i in 1 2 3 4 # defect 5
+#do
+#	echo "###################### demo$i #######################"
+#	(
+#		echo make -f Makefile.ac AIRCRAFT=DEMO clean_ac demo$i.compile > /dev/tty
+#		make -f Makefile.ac AIRCRAFT=DEMO clean_ac demo$i.compile
+#	) 2>&1 | egrep Total\|Error\|Fehler
+#done
